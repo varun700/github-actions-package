ce08a2a4af39118709d76b227b2046ad2dee9653
varun700 <varunmgeakminds@gmail.com>
2022-06-13 17:17:02 +0530

    component changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,12 @@
+// module.exports = {
+//   stories: ["./*.stories.js"],
+// };
 module.exports = {
   stories: ["./*.stories.js"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+  ],
 };
---
+++ src/Simplebutton/Simplebutton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Simplebutton = ({ content }) => {
+  return <button>{content}</button>;
+};
+
+export default Simplebutton;
